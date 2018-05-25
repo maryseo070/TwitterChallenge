c30df3d2924fea86c4f8654f8c560515caeb3c54
maryseo070 <maryseo070@gmail.com>
2018-05-25 10:29:21 -0400

    figaro install

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -6,12 +6,17 @@ class Api::ClientsController < ApplicationController
 
   def index
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "PLFs6Ji5TLhXLihAW59Xpsk69"
-      config.consumer_secret     = "NVt3JLeAj1aBHns1JYwios1H17fd0eqdK1hUE9wzAg8FZDhozI"
-      config.access_token        = "999983451481759745-OjsaXhJQyqpRfHPMeiVyxugkWjxiR5P"
-      config.access_token_secret = "To4VvdetNYdwuqs4Pdxa3DuWOm9frJliljf8oOxU11bQi"
+      config.consumer_key        = ENV["consumer_key"]
+      config.consumer_secret     = ENV["consumer_secret"]
+      config.access_token        = ENV["access_token"]
+      config.access_token_secret = ENV["access_token_secret"]
     end
     tweets = @client.user_timeline('rubyinside', count: 25)
+
     debugger
   end
+
+
+
+
 end
